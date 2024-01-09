0dd03798ebb3ddc9b19a57ebc8f9e7245146834d
TheGlassMan <netojr334@gmail.com>
2024-01-09 16:33:50 -0700

    idk

---
+++ Labs/Lab0/cmake-build-debug/CMakeFiles/Lab0.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab0.dir/lab0.c.obj"
+  "CMakeFiles/Lab0.dir/lab0.c.obj.d"
+  "Lab0.exe"
+  "Lab0.exe.manifest"
+  "Lab0.pdb"
+  "libLab0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
